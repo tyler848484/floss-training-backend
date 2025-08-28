b430a30362f0137b9299f2f103acef6359a717ba
Tyler Floss <tylerfloss@gmail.com>
2025-08-28 12:52:48 -0500

    add db table creation on startup

--- app/main.py
+++ app/main.py
@@ -3,10 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import parents, children, sessions, bookings, reviews, auth
 from starlette.middleware.sessions import SessionMiddleware
 import os
+from app.db import Base, engine
 
 app = FastAPI(
-    title="Private Soccer Training API",
-    description="Backend API for private soccer training booking system",
+    title="Floss Private Soccer Coaching API",
+    description="Backend API for private soccer coaching booking system",
     version="1.0.0"
 )
 
@@ -20,8 +21,12 @@ app.add_middleware(
 
 app.add_middleware(
     SessionMiddleware,
-    secret_key=os.getenv("SESSION_SECRET_KEY", "your-very-secret-random-string"),
+    secret_key=os.getenv("SESSION_SECRET_KEY"),
 )
+
+@app.on_event("startup")
+def create_tables_on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
